774e6f0ed549d7a8130328aed17604b99dbc1937
unknown <devrafaelcoelho@gmail.com>
2024-03-17 11:39:36 -0300

    created signup form

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,11 +1,17 @@
 import { Typography } from '@mui/material';
+import SignupForm from '../../components/Forms/SignupForm';
 import Header from '../../components/Header';
 
 export default function Signup() {
   return (
     <>
       <Header />
-      <Typography variant="h3">Signup</Typography>
+
+      <Typography variant="h4" align="center" mt={4} mb={4}>
+        ADD YOUR DATA
+      </Typography>
+
+      <SignupForm />
     </>
   );
 }
