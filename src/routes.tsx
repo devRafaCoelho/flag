47fe24d517682003d30cf575db7ce57310f49b5e
unknown <devrafaelcoelho@gmail.com>
2024-03-21 22:16:11 -0300

    review

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,13 +4,10 @@ import Events from './pages/Events';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import MyAccount from './pages/MyAccount';
-import SeriesA from './pages/SeriesA';
-import SeriesB from './pages/SeriesB';
-import SeriesC from './pages/SeriesC';
+import Ranking from './pages/Ranking';
 import Signup from './pages/Signup';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { getTheme } from './theme/theme';
-import Ranking from './pages/Ranking';
 
 export default function MainRoutes() {
   const theme = getTheme();
@@ -23,9 +20,6 @@ export default function MainRoutes() {
         <Route path="/home" element={<Home />} />
         <Route path="/ranking" element={<Ranking />} />
         <Route path="/events" element={<Events />} />
-        <Route path="/seriesA" element={<SeriesA />} />
-        <Route path="/seriesB" element={<SeriesB />} />
-        <Route path="/seriesC" element={<SeriesC />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/myAccount" element={<MyAccount />} />
